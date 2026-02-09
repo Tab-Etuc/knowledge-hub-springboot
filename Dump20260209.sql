59b73f4ffed5b204f12cbd7834482ce1b710ca94
羅少希 <xji.gl4.vu@gmail.com>
2026-02-09 09:28:25 +0800

    fix: 補上 academic_book_detail 資料表

--- Dump20260209.sql
+++ Dump20260209.sql
@@ -171,3 +171,38 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2026-02-09  1:42:14
+
+--
+-- Table structure for table `academic_book_detail`
+--
+
+DROP TABLE IF EXISTS `academic_book_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `academic_book_detail` (
+  `isbn` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lc_class_mark` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`isbn`),
+  CONSTRAINT `fk_academic_book_isbn` FOREIGN KEY (`isbn`) REFERENCES `book` (`isbn`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `academic_book_detail`
+--
+
+LOCK TABLES `academic_book_detail` WRITE;
+/*!40000 ALTER TABLE `academic_book_detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `academic_book_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-09  1:36:37
